c2f76d2c702ad7f9173b9f7ab2b67e1ea60666df
devMonkRahul <rahulpal2023iem@gmail.com>
2025-08-31 23:08:27 +0530

    feat: implement message handling APIs for accepting, sending, and suggesting messages

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,65 @@
+import dbConnect from "@/config/connectDB";
+import UserModel, { Message } from "@/model/User.model";
+
+export async function POST(request: Request) {
+    await dbConnect();
+
+    const { username, content } = await request.json();
+
+    try {
+        const user = await UserModel.findOne({ username });
+
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                {
+                    status: 404,
+                }
+            )
+        }
+
+        if (!user.isAcceptingMessages) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User is not accepting messages",
+                    },
+                {
+                    status: 403,
+                }
+            );
+        }
+
+        const newMessage = {
+            content,
+            createdAt: new Date(),
+        }
+
+        user.messages.push(newMessage as Message);
+        await user.save();
+
+        return Response.json(
+            {
+                success: true,
+                message: "Message sent successfully",
+            },
+            {
+                status: 200,
+            }
+        );
+    } catch (error) {
+        console.error("Error in Sending Message: ", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Error in Sending Message",
+            },
+            {
+                status: 500,
+            }
+        );
+    }
+}
